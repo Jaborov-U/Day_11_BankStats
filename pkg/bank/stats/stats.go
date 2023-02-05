c427f6b7eecadd0651d2e94e28945676c0cfaf17
Jaborov-U <umed.jaborov.zz@gmail.com>
2023-02-05 10:16:37 +0500

    added function PeriodsDynamic

--- pkg/bank/stats/stats.go
+++ pkg/bank/stats/stats.go
@@ -51,3 +51,28 @@ func CategoriesAvg(payments []types.Payment) map[types.Category]types.Money{
 
     return categories
 }
+
+// PeriodsDynamic сравнивает расходы по категориям за два периода
+func PeriodsDynamic(first map[types.Category]types.Money, second map[types.Category]types.Money) map[types.Category]types.Money{
+
+	periodDinamic := map[types.Category]types.Money{}
+
+	for key, value  := range second {
+		_, ok := first[key]
+		if !ok {
+			periodDinamic[key] = value
+			continue
+		} 
+		periodDinamic[key] = value - first[key]
+	}
+
+	for category  := range first {
+		_, ok := second[category]
+		if !ok {
+			periodDinamic[category] = 0
+			continue
+		} 
+	}
+	
+	return periodDinamic
+}
--- pkg/bank/stats/stats_test.go
+++ pkg/bank/stats/stats_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"reflect"
 	"testing"
+
 	"github.com/Jaborov-U/Day-11-BankTypes/v2/pkg/types"
 )
 
@@ -35,27 +36,27 @@ func ExampleTotalInCategory() {
 
 	cards := []types.Payment{
 		{
-			Amount: 10,
+			Amount:   10,
 			Category: "Shop",
-			Status: types.StatusFail,
+			Status:   types.StatusFail,
 		},
 		{
-			Amount: 15,
+			Amount:   15,
 			Category: "Shop",
-			Status: types.StatusInProgress,
+			Status:   types.StatusInProgress,
 		},
 		{
-			Amount: 15,
+			Amount:   15,
 			Category: "Internet",
-			Status: types.StatusOk,
+			Status:   types.StatusOk,
 		},
 	}
 	avgPays := TotalInCategory(cards, "Shop")
 
 	fmt.Println(avgPays)
-	
+
 	//Output: 15
-	
+
 }
 
 func TestCategoriesAvg_Test(t *testing.T) {
@@ -68,13 +69,40 @@ func TestCategoriesAvg_Test(t *testing.T) {
 
 	expected := map[types.Category]types.Money{
 		"internet": 10,
-		"taxi": 15,
+		"taxi":     15,
 	}
 
 	result := CategoriesAvg(payments)
 
-	if !reflect.DeepEqual(expected, result){
+	if !reflect.DeepEqual(expected, result) {
 		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
 	}
 
+}
+
+func TestPeriodDinamic_Succes(t *testing.T) {
+	first := map[types.Category]types.Money{
+		"auto":   10,
+		"food":   20,
+		"mobile": 10,
+	}
+
+	second := map[types.Category]types.Money{
+		"auto": 5,
+		"food": 3,
+		"taxi": 5,
+	}
+
+	expected := map[types.Category]types.Money{
+		"auto":   -5,
+		"food":   -17,
+		"taxi":   5,
+		"mobile": 0,
+	}
+
+	periodDinamic := PeriodsDynamic(first, second)
+
+	if !reflect.DeepEqual(periodDinamic, expected) {
+		t.Errorf("result: %v, expected: %v", periodDinamic, expected)
+	}
 }
